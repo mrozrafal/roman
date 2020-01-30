4399d656781b7f52605caf0823a5bb3cccfcb6b6
Rafal Mroz <rafalmroz30@gmail.com>
2020-01-30 16:09:21 +0100

    feat: tests for numbers between 1-3999

--- src/toRoman.test.js
+++ src/toRoman.test.js
@@ -4,7 +4,11 @@ import toRoman from './toRoman'
 describe('toRoman()', () => {
     it.each([
         [1, "I"],
-        [5, "V"]
+        [5, "V"],
+        [10, "X"],
+        [200, "CC"],
+        [474, "CDLXXIV"],
+        [2019, "MMXIX"]
         
 
     ])('converts %d to %s', (arabic, expectedRoman)=>{
@@ -12,7 +16,10 @@ describe('toRoman()', () => {
     });
     
     it('does not convert to any roman number 0', () => {
-        expect(toRoman(0)).toEqual("none")
+        expect(toRoman(0)).toEqual("Enter a number between 1 and 3999")
+    })
+    it('does not convert to any roman number above 3999', () => {
+        expect(toRoman(0)).toEqual("Enter a number between 1 and 3999")
     })
 });
 
