d9837d684d239b5b8de6df54337d717b7b672719
Rafal Mroz <rafalmroz30@gmail.com>
2020-03-10 18:58:29 +0100

    Updates

---
+++ precache-manifest.4eb0857bddf77a8225169787290dcd1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a15adefa565b3c19a880cceb950b177",
+    "url": "/roman/index.html"
+  },
+  {
+    "revision": "351e003b9f906da41353",
+    "url": "/roman/static/css/main.81a42ae2.chunk.css"
+  },
+  {
+    "revision": "0833bc570a8856b175bf",
+    "url": "/roman/static/js/2.c7841c71.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/roman/static/js/2.c7841c71.chunk.js.LICENSE"
+  },
+  {
+    "revision": "351e003b9f906da41353",
+    "url": "/roman/static/js/main.57665469.chunk.js"
+  },
+  {
+    "revision": "34c0512249d257cdf6d4",
+    "url": "/roman/static/js/runtime-main.a2e5d973.js"
+  },
+  {
+    "revision": "c364365e3b57895517ab566cb83c52b3",
+    "url": "/roman/static/media/roman-converter.c364365e.png"
+  }
+]);
